4587b47222e9c71c28e1f63aebaee85bd360e3d1
JaviEsgueva <118816407+JaviEsgueva@users.noreply.github.com>
2023-01-10 13:20:25 +0100

    Primera confirmacion

---
+++ Ejercicio1_Tema4/src/ejercicio1_tema4/votacion.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1_tema4;
+
+/**
+ *
+ * @author esgsanja
+ */
+public class votacion {
+    private String nombre;
+    private Integer votos;
+
+    public votacion() {
+    }
+
+    public votacion(String nombre, Integer votos) {
+        this.nombre = nombre;
+        this.votos = votos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getVotos() {
+        return votos;
+    }
+
+    public void setVotos(Integer votos) {
+        this.votos = votos;
+    }
+
+    @Override
+    public String toString() {
+        return "votacion{" + "nombre=" + nombre + ", votos=" + votos + '}';
+    }
+    
+    
+}
